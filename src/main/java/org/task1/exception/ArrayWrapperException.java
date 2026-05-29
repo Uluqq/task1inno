d96b25da9c831522905c3551d5687107712a38a8
Uluqq <papkovice@gmail.com>
2026-05-29 18:12:55 +0300

    Added custom Exception

---
+++ src/main/java/org/task1/exception/ArrayWrapperException.java
@@ -0,0 +1,16 @@
+package org.task1.exception;
+
+public class ArrayWrapperException extends Exception {
+    public ArrayWrapperException(){
+        super();
+    }
+    public ArrayWrapperException(String message){
+        super(message);
+    }
+    public ArrayWrapperException(Throwable cause){
+        super(cause);
+    }
+    public ArrayWrapperException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
