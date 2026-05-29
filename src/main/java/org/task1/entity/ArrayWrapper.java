283da60d87704f93f428078ba273ca7b45e39a19
Uluqq <papkovice@gmail.com>
2026-05-29 18:03:22 +0300

    init commit + entity

---
+++ src/main/java/org/task1/entity/ArrayWrapper.java
@@ -0,0 +1,43 @@
+package org.task1.entity;
+
+import java.util.Arrays;
+
+public class ArrayWrapper {
+    private int[] array;
+
+    public ArrayWrapper(int[] array){
+        if (array != null){
+            this.array = Arrays.copyOf(array, array.length);
+        } else {
+            this.array = null;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ArrayWrapper that = (ArrayWrapper) o;
+        return Arrays.equals(array, that.array);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(array);
+    }
+
+    @Override
+    public String toString() {
+        return "ArrayWrapper{" +
+                "array=" + Arrays.toString(array) +
+                '}';
+    }
+
+    public int[] getArray() {
+        if (array != null){
+            return Arrays.copyOf(array, array.length);
+        } else {
+            return null;
+        }
+    }
+}
